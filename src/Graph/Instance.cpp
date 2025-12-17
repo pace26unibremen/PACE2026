e326d2fd556659ca5617648c69a701bb4844f8b1
jschramm <jschramm@uni-bremen.de>
2025-12-17 13:59:58 +0100

    add write and dot methods for instance

--- src/Graph/Instance.cpp
+++ src/Graph/Instance.cpp
@@ -53,3 +53,88 @@ std::shared_ptr<graph::Instance> graph::ReadInstance(const std::filesystem::path
     }
     return result;
 }
+
+void graph::WriteInstance(const std::shared_ptr<Instance>& instance, std::ostream& os)
+{
+    os << "#p " << instance->size() << " ";
+    if (not instance->empty())
+    {
+        os << instance->at(0)->Terminals().size();
+    }
+    else
+    {
+        os << " ";
+    }
+    os << "\n";
+
+    for(const auto& forest : *instance)
+    {
+        forest->write(os);
+    }
+}
+
+void graph::WriteInstance(const std::shared_ptr<Instance>& instance, const std::filesystem::path& path)
+{
+    std::ofstream outStream(path);
+    if (!outStream.is_open())
+    {
+        throw std::invalid_argument("Instance : WriteInstance : couldn't open file");
+    }
+    WriteInstance(instance ,outStream);
+    outStream.close();
+}
+
+void graph::DotInstance(const std::shared_ptr<Instance>& instance, std::ostream& os)
+{
+    os << "digraph Instance {\n"
+       << "splines = false\n\n";
+
+    for (unsigned int i = 0; i < instance->size(); ++i)
+    {
+
+        os << "subgraph cluster_" << i << " {\n"
+           << "    style = dotted;\n"
+           << "    node [\n"
+           << "        shape = circle,\n"
+           << "        fontsize = 15,\n"
+           << "        label = \"\",\n"
+           << "        height = 0.1,\n"
+           << "        fillcolor = \"#00000022\",\n"
+           << "        style = filled,\n"
+           << "        fixedsize = true,\n"
+           << "        labelloc = t];\n"
+           << "    edge [arrowhead = none];\n\n";
+
+        os << "    inv_" << i << "[style = invis];\n\n";
+
+        const auto& forest = instance->at(i);
+        for (auto t : forest->Terminals())
+        {
+            os << "    n_" << i << "_" << t.first << " [label = \"" << t.second << "\\n\\n\\n \"];\n";
+            os << "    n_" << i << "_" << t.first << " -> inv_" << i << "[style = invis];\n";
+        }
+        for (size_t n = 0; n < forest->Nodes().size(); ++n)
+        {
+            const Node& node = forest->Nodes()[n];
+            if(node.leftChildIndex != -1)
+                os << "    n_" << i << "_" << n << " -> n_" << i << "_" << node.leftChildIndex << ";\n";
+            if(node.rightChildIndex != -1)
+                os << "    n_" << i << "_" << n << " -> n_" << i << "_" << node.rightChildIndex << ";\n";
+        }
+
+        os << "}\n\n";
+    }
+
+    os << "}" << std::endl;
+}
+
+void graph::DotInstance(const std::shared_ptr<Instance>& instance, const std::filesystem::path& path)
+{
+    std::ofstream outStream(path);
+    if (!outStream.is_open())
+    {
+        throw std::invalid_argument("Instance : DotInstance : couldn't open file");
+    }
+    DotInstance(instance ,outStream);
+    outStream.close();
+}
--- src/Graph/Instance.hpp
+++ src/Graph/Instance.hpp
@@ -8,6 +8,14 @@ namespace graph
     typedef std::vector<std::shared_ptr<Forest>> Instance;
 
     std::shared_ptr<Instance> ReadInstance(const std::filesystem::path& path);
+
+    void WriteInstance(const std::shared_ptr<Instance>& instance, std::ostream& os);
+
+    void WriteInstance(const std::shared_ptr<Instance>& instance, const std::filesystem::path& path);
+
+    void DotInstance(const std::shared_ptr<Instance>& instance, std::ostream& os);
+
+    void DotInstance(const std::shared_ptr<Instance>& instance, const std::filesystem::path& path);
 }
 
 #endif  //PACE2026_INSTANCE_HPP
