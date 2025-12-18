de36e6d0ac238093552414d588d5f5ab412da15b
André Kaufmann <an_ka1@uni-bremen.de>
2025-12-18 11:59:01 +0100

    Early Rendition of the Chain Identification.

---
+++ src/Solver/Rule/ChainReductionRule.h
@@ -0,0 +1,19 @@
+//
+// Created by kaufm on 11.12.2025.
+//
+
+#ifndef PACE2026_CHAINREDUCTIONRULE_H
+#define PACE2026_CHAINREDUCTIONRULE_H
+#include "AbstractRule.hpp"
+
+namespace solver
+{
+    class ChainReductionRule : public AbstractRule
+    {
+
+        ChainReductionRule();
+    };
+
+
+}
+#endif  //PACE2026_CHAINREDUCTIONRULE_H
