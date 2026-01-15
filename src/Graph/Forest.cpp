f688a3c323758dc4c6defca42f40c353792fbfd3
Leon Flaack <lflaack@protonmail.com>
2026-01-16 00:24:54 +0100

    Resolve double definition of Forest::hasIdenticalSubtree

--- src/Graph/Forest.cpp
+++ src/Graph/Forest.cpp
@@ -483,53 +483,6 @@ bool Forest::checkTriple(int parentIndex, std::unordered_map<int, unsigned int>&
     return tripleValid;
 }
 
-bool Forest::hasIdenticalSubtree(const Forest& other, int thisNodeIdx, int otherNodeIdx)
-{
-    std::stack<int> thisVisitingStack;
-    thisVisitingStack.push(thisNodeIdx);
-    std::stack<int> otherVisitingStack;
-    otherVisitingStack.push(otherNodeIdx);
-
-    while (not (thisVisitingStack.empty() or otherVisitingStack.empty()))
-    {
-        Node& thisCurrentNode = nodes->at(thisVisitingStack.top());
-        thisVisitingStack.pop();
-        Node& otherCurrentNode = other.nodes->at(otherVisitingStack.top());
-        otherVisitingStack.pop();
-
-        if (not thisCurrentNode.hasSameTerminals(otherCurrentNode))
-        {
-            return false;
-        }
-
-        if (not (thisCurrentNode.firstChildIndex == -1 or otherCurrentNode.secondChildIndex == -1)) //TODO: assume both child indices -1 if one is empty?
-        {
-            // neither is a leaf node
-            thisVisitingStack.push(thisCurrentNode.secondChildIndex);
-            thisVisitingStack.push(thisCurrentNode.firstChildIndex);
-            otherVisitingStack.push(otherCurrentNode.secondChildIndex);
-            otherVisitingStack.push(otherCurrentNode.firstChildIndex);
-        }
-        else
-        {
-            // one or both are leaves
-            if (not (thisCurrentNode.firstChildIndex == -1 and otherCurrentNode.secondChildIndex == -1))
-            {
-                // just one is a leaf
-                return false;
-            }
-        }
-
-
-    }
-    if (thisVisitingStack.empty() and otherVisitingStack.empty())
-    {
-        return true;
-    }
-    return false;
-}
-
-
 // ------------------------------------------------------------- //
 // ---- graph manipulation ------------------------------------- //
 // ------------------------------------------------------------- //
