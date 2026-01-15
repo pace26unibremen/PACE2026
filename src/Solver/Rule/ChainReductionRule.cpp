fad98e6da755f83fbc314d7894bf07a0ba7c119c
André Kaufmann <an_ka1@uni-bremen.de>
2026-01-15 21:38:25 +0100

    Refactoring to be in accordance to how rules are set up at the moment

--- src/Solver/Rule/ChainReductionRule.cpp
+++ src/Solver/Rule/ChainReductionRule.cpp
@@ -1,5 +1,5 @@
 //
-// Created by kaufm on 11.12.2025.
+// Created by Andre on 11.12.2025.
 //
 
 #include "ChainReductionRule.h"
@@ -7,90 +7,73 @@
 #include <algorithm>
 #include <iostream>
 #include <list>
+#include <stack>
 #include <string>
 
-solver::ChainReductionRule::ChainReductionRule()
+solver::ChainReductionRule::ChainReductionRule(
+    const std::shared_ptr<graph::Forest>& T1,
+    const std::shared_ptr<graph::Forest>& T2,
+    const std::vector<std::list<int>>& chains
+    )
 {
     //Copying of the Trees maybe irrelevant when doing this without const params. Not sure.
+    this->T1 = T1;
+    this->T2 = T2;
+    this->chains = chains;
+    std::stack<AbstractRule> changes = std::stack<AbstractRule>();
+}
 
+//Chain def:Let T be a rooted phylogenetic X-tree, and let C =
+//(x1, x2, . . . , xn) be a sequence of elements in X with n ≥ 2.
+//X:= Set of labels, bijectively used on leaves in the tree.
+
+//We say that C is an n-chain (or short chain) of T
+//if the parent of x1 coincides with the parent of x2
+//or the parent of x2 is the parent of the parent of x1,
+//and, for each i ∈ {3, 4, . . . , n},
+//the parent of xi is the parent of the parent of xi−1.
+
+
+void solver::ChainReductionRule::apply()
+{
+    if (this->isApplied)
+    {
+        throw std::invalid_argument("ChainReductionRule : apply : rule was already applied");
+    }
+    isApplied = true;
 
 }
 
-//Wie will man es machen?
-//von Anzahl der Nodes bis 4...
-// Zuerster Check: Gibt es zwei Nodes, welche Eltern teilen
-// Dann: Aus den verbleibenen Nodes: Für die verbleibenen Nodes: Existiert bei jeder Runde ein Knoten, welcher der Parent
-// des Parents der beiden knoten ist?
-
-// int sizeOfSharedNodes(std::vector<graph::Node> t1Node, std::vector<graph::Node> t2Node)
-// {
-//     int highestSize = 0;
-//
-//     for (unsigned int i = 0; i < t1Node.size();  i++)
-//     {
-//         for (unsigned int j = 0; j < t2Node.size(); j++)
-//         {
-//             // if () WARTE BIS ADDRESSIERUNG DER NODES VERBESSERT WIRD
-//             // {
-//             //     highestSize++;
-//             // }
-//         }
-//     }
-//
-//     return highestSize;
-// }
-//Forest stores all vertex with a nummer label. TerminalIndexToLabel orders the index of the leafs/terminals to the
-//label, LabelToTerminalIndex the opposite.
-
-
-
-
-
-
-//SO: First step: Identify Chains of n>= 4 that are present in T and T'.
-//Second Step: Apply the reduction to both Forests for each chain.
-
-//First step: Go through all element pairs with size n >= 4 on both T and T'
-// IFF a Chain that is common in T and T' is found, then apply the subtree/chain deletion by removing the subtree.
-// (I think that atm removing the edge between the parent of the node of the to the removed subtree/chain is the way to
-// do that)
-// Do this for all possible pairs and either quit the func if both trees were accessible during execution of func, or
-// return both now reduced trees.
-
-
-//Func to return all combinations
-
-//we need to do this, since if a node is unique to only one, then it can't be removed.
-
-void ChainReductionExecution(std::shared_ptr<graph::Forest>& T1, std::shared_ptr<graph::Forest>& T2)
+void solver::ChainReductionRule::unapply()
 {
 
+}
+
+std::shared_ptr<solver::AbstractRule>
+solver::ChainReductionRule::isApplicable(const std::shared_ptr<graph::Forest>& T1,
+    const std::shared_ptr<graph::Forest>& T2)
+{
+    //List of chains in the tree
+    std::vector<std::list<int>> chainList;
+
     int size;
-    if (T1->Nodes().size() > T2->Nodes().size())
-    {
-        size = T1->Nodes().size();
-    }
-    else
-    {
-        size = T2->Nodes().size();
-    }
 
     //Fetch Nodes
     std::vector<graph::Node> treeOneNodes = T1->Nodes();
     std::vector<graph::Node> treeTwoNodes = T2->Nodes();
 
-    // //Fetch Terminals
-    // std::unordered_map<int,unsigned int> termIndexTreeOne = T1->Terminals();
-    // std::unordered_map<int,unsigned int> termIndexTreeTwo = T2->Terminals();
+    //Fetch Leaves - Terminal Index to Label
+    std::unordered_map<int,unsigned int> termIndexTreeOne = T1->Terminals();
+    std::unordered_map<int,unsigned int> termIndexTreeTwo = T2->Terminals();
 
-    //Chain def:Let T be a rooted phylogenetic X-tree, and let C =
-    //(x1, x2, . . . , xn) be a sequence of elements in X with n ≥ 2.
-
-    //We say that C is an n-chain (or short chain) of T if the
-    //parent of x1 coincides with the parent of x2 or the parent
-    //of x2 is the parent of the parent of x1, and, for each
-    //i ∈ {3, 4, . . . , n}, the parent of xi is the parent of the parent
-    //of xi−1.
+    if (termIndexTreeOne.size() < termIndexTreeTwo.size())
+    {
+        size = termIndexTreeTwo.size();
+    }
+    else
+    {
+        size = termIndexTreeOne.size();
+    }
 
     //For every node that could be shared between both...
     for (int currentNodeIndex = 0; currentNodeIndex < size; currentNodeIndex++)
@@ -210,12 +193,13 @@ void ChainReductionExecution(std::shared_ptr<graph::Forest>& T1, std::shared_ptr
             }
 
             //Execute removal of chain from x4 onwards
+            if (commonChainNodes.size() >= 4)
+            {
+
+            }
         }
     }
-
     // int maximumSharedNodes = sizeOfSharedNodes(treeOneNodes,treeTwoNodes);
-
-
     //Enumerate over all
 
 }
