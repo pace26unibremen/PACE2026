cbcfa94584a4dea4f9e057dfbbf9bbdfff15025a
André Kaufmann <an_ka1@uni-bremen.de>
2026-01-15 21:38:25 +0100

    Refactoring to be in accordance to how rules are set up at the moment

--- src/Solver/Rule/ChainReductionRule.h
+++ src/Solver/Rule/ChainReductionRule.h
@@ -5,43 +5,27 @@
 #ifndef PACE2026_CHAINREDUCTIONRULE_H
 #define PACE2026_CHAINREDUCTIONRULE_H
 #include "AbstractRule.hpp"
-#include "../Action/DeleteNodeActionInChains.h"
-#include "../Action/DeleteEdgeAction.hpp"
+
+#include <iosfwd>
+#include <list>
 #include <vector>
-#include <stack>
 
 namespace solver
 {
     class ChainReductionRule : public AbstractRule
     {
-    protected:
-        /// \brief First Tree
         std::shared_ptr<graph::Forest> T1;
-
-        /// \brief Second Tree
         std::shared_ptr<graph::Forest> T2;
+        std::vector<std::list<int>> chains;
 
-        /// \brief First Chain found within the two trees
-        std::vector<std::vector<int>> chain;
+        ChainReductionRule(const std::shared_ptr<graph::Forest>& T1, const std::shared_ptr<graph::Forest>& T2,
+                           const std::vector<std::list<int>>& chains);
+        void apply();
 
-        /// \brief Amount of changes done
-        std::stack<solver::DeleteNodeActionInChains> changes;
+        void unapply();
 
-    public:
-        /// \param T1 First Tree
-        /// \param T2 Second Tree
-        /// \param T3 Chains between those two trees in the paramater
-
-       ChainReductionRule(const std::shared_ptr<graph::Forest>& T1, const std::shared_ptr<graph::Forest>& T2,
-                         const std::vector<std::vector<int>>& chains);
-      /// \brief Apply the Chain Reduction rule onto the two Trees
-        void apply() override;
-        /// \brief Restore the original state before the application of the chain reduction rule
-        void unapply() override;
-
-        /// \brief Determine if there are chains between the two trees.
-        static std::shared_ptr<AbstractRule> isApplicable(const std::shared_ptr<graph::Forest>& T1,
-            const std::shared_ptr<graph::Forest>& T2);
+        std::shared_ptr<solver::AbstractRule> isApplicable(const std::shared_ptr<graph::Forest>& T1,
+    const std::shared_ptr<graph::Forest>& T2);
     };
 
 
