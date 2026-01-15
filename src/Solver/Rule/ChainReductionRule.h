d90f1729fe083c2d923de9fbf08d66ed8599e6ad
André Kaufmann <an_ka1@uni-bremen.de>
2026-01-15 21:38:25 +0100

    Refactoring to be in accordance to how rules are set up at the moment

--- src/Solver/Rule/ChainReductionRule.h
+++ src/Solver/Rule/ChainReductionRule.h
@@ -6,12 +6,26 @@
 #define PACE2026_CHAINREDUCTIONRULE_H
 #include "AbstractRule.hpp"
 
+#include <iosfwd>
+#include <list>
+#include <vector>
+
 namespace solver
 {
     class ChainReductionRule : public AbstractRule
     {
+        std::shared_ptr<graph::Forest> T1;
+        std::shared_ptr<graph::Forest> T2;
+        std::vector<std::list<int>> chains;
 
-        ChainReductionRule();
+        ChainReductionRule(const std::shared_ptr<graph::Forest>& T1, const std::shared_ptr<graph::Forest>& T2,
+                           const std::vector<std::list<int>>& chains);
+        void apply();
+
+        void unapply();
+
+        std::shared_ptr<solver::AbstractRule> isApplicable(const std::shared_ptr<graph::Forest>& T1,
+    const std::shared_ptr<graph::Forest>& T2);
     };
 
 
