e3aafae0be3ab9a8afcdaefc2dffb81f1cbb009d
André Kaufmann <an_ka1@uni-bremen.de>
2026-01-23 00:06:30 +0100

    Implementation of Case 1 for Chains

--- src/Solver/Rule/ChainReductionRule.h
+++ src/Solver/Rule/ChainReductionRule.h
@@ -5,27 +5,27 @@
 #ifndef PACE2026_CHAINREDUCTIONRULE_H
 #define PACE2026_CHAINREDUCTIONRULE_H
 #include "AbstractRule.hpp"
-
-#include <iosfwd>
-#include <list>
+#include "../Action/DeleteEdgeAction.hpp"
 #include <vector>
 
 namespace solver
 {
     class ChainReductionRule : public AbstractRule
     {
+    protected:
         std::shared_ptr<graph::Forest> T1;
         std::shared_ptr<graph::Forest> T2;
-        std::vector<std::list<int>> chains;
+        std::vector<std::vector<std::vector<int>>> chains;
 
+    public:
         ChainReductionRule(const std::shared_ptr<graph::Forest>& T1, const std::shared_ptr<graph::Forest>& T2,
-                           const std::vector<std::list<int>>& chains);
-        void apply();
+                           const std::vector<std::vector<std::vector<int>>>& chains);
+        void apply() override;
 
-        void unapply();
+        void unapply() override;
 
-        std::shared_ptr<solver::AbstractRule> isApplicable(const std::shared_ptr<graph::Forest>& T1,
-    const std::shared_ptr<graph::Forest>& T2);
+        static std::shared_ptr<AbstractRule> isApplicable(const std::shared_ptr<graph::Forest>& T1,
+            const std::shared_ptr<graph::Forest>& T2);
     };
 
 
