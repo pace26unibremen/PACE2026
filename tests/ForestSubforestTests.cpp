ea50975bf8824a113c5dd5d301c0f4ffe476d8de
Alexander Wachowski <a.wachowski@tutanota.com>
2026-01-22 16:51:39 +0100

    Refactor tests to include numberOfTerminals in creation of trees

--- tests/ForestSubforestTests.cpp
+++ tests/ForestSubforestTests.cpp
@@ -39,7 +39,7 @@ TEST_CASE("Sub Forest for simple trees with two terminals", "[Forest, sub forest
     SECTION("Check sub forest on two simple trees")
     {
         auto t1 = graph::Forest(std::string(TEST_EXAMPLES_DIR) + "tree_1_2_simple.tree");
-        auto f2 = graph::Forest(std::string(TEST_EXAMPLES_DIR) + "forest_2_2_singleVertexTrees.tree", 0, 2);
+        auto f2 = graph::Forest(std::string(TEST_EXAMPLES_DIR) + "forest_2_2_singleVertexTrees.tree", 2, 2);
 
         REQUIRE(f2.isTrueSubtreeOf(t1));
         REQUIRE_FALSE(t1.isTrueSubtreeOf(f2));
@@ -52,7 +52,7 @@ TEST_CASE("Sub Forest", "[Forest, sub forest, <=]")
     SECTION("Check sub forest - example 1")
     {
         auto t1 = graph::Forest(std::string(TEST_EXAMPLES_DIR) + "tree_1_6_example1.tree");
-        auto f2 = graph::Forest(std::string(TEST_EXAMPLES_DIR) + "forest_4_6_simple2.tree", 0, 4);
+        auto f2 = graph::Forest(std::string(TEST_EXAMPLES_DIR) + "forest_4_6_simple2.tree", 6, 4);
 
         REQUIRE(f2.isTrueSubtreeOf(t1));
         REQUIRE_FALSE(t1.isTrueSubtreeOf(f2));
@@ -61,7 +61,7 @@ TEST_CASE("Sub Forest", "[Forest, sub forest, <=]")
     SECTION("Check sub forest - example 2")
     {
         auto t1 = graph::Forest(std::string(TEST_EXAMPLES_DIR) + "tree_1_6_example2.tree");
-        auto f2 = graph::Forest(std::string(TEST_EXAMPLES_DIR) + "forest_3_6_simple.tree", 0, 3);
+        auto f2 = graph::Forest(std::string(TEST_EXAMPLES_DIR) + "forest_3_6_simple.tree", 6, 3);
 
         REQUIRE(f2.isTrueSubtreeOf(t1));
         REQUIRE_FALSE(t1.isTrueSubtreeOf(f2));
