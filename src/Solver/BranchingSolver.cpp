a665bfa5469eefd94c193e011391217ae5f5dec3
jschramm <jschramm@uni-bremen.de>
2026-01-29 12:55:43 +0100

    integrate debug plugin in branching solver

--- src/Solver/BranchingSolver.cpp
+++ src/Solver/BranchingSolver.cpp
@@ -13,6 +13,11 @@ solver::BranchingSolver::BranchingSolver(const std::shared_ptr<graph::Instance>&
     : AbstractSolver(instance)
 {}
 
+std::shared_ptr<solver::DebugPlugin>& solver::BranchingSolver::DebPlugin()
+{
+    return this->debPlugin;
+}
+
 std::shared_ptr<graph::Forest> solver::BranchingSolver::solve()
 {
     std::vector<std::function<std::shared_ptr<AbstractRule>(std::shared_ptr<graph::Instance> instance)>>
@@ -24,6 +29,8 @@ std::shared_ptr<graph::Forest> solver::BranchingSolver::solve()
                            solver::DebugAssertFalseRule::isApplicable};
 
     std::shared_ptr<graph::Forest> solution = nullptr;
+
+    if (debPlugin) debPlugin->init(instance);
 
     while (true)
     {
@@ -37,11 +44,13 @@ std::shared_ptr<graph::Forest> solver::BranchingSolver::solve()
                     temporalChanges.top()->unapply();
                     temporalChangesCopy.push(temporalChanges.top());
                     temporalChanges.pop();
+                    if (debPlugin) debPlugin->onTempUnapply(temporalChanges.top(), temporalChanges.empty());
                 }
                 solution = std::make_shared<graph::Forest>((*instance->at(0)).copy());
                 while (not temporalChangesCopy.empty())
                 {
                     temporalChangesCopy.top()->apply();
+                    if (debPlugin) debPlugin->onTempApply(temporalChangesCopy.top());
                     temporalChanges.push(temporalChangesCopy.top());
                     temporalChangesCopy.pop();
                 }
@@ -50,6 +59,7 @@ std::shared_ptr<graph::Forest> solver::BranchingSolver::solve()
             {
                 if (changes.empty())
                 {
+                    if (debPlugin) debPlugin->onEnd();
                     return solution;
                 }
                 auto rule = changes.top();
@@ -58,6 +68,7 @@ std::shared_ptr<graph::Forest> solver::BranchingSolver::solve()
                     if (branchingRule->isFullyExplored())
                     {
                         branchingRule->unapply();
+                        if (debPlugin) debPlugin->onUnapply(rule);
                         changes.pop();
                         if (not temporalChanges.empty() and temporalChanges.top() == branchingRule)
                         {
@@ -67,13 +78,16 @@ std::shared_ptr<graph::Forest> solver::BranchingSolver::solve()
                     else
                     {
                         branchingRule->unapply();
+                        if (debPlugin) debPlugin->onUnapply(rule);
                         branchingRule->apply();
+                        if (debPlugin) debPlugin->onApply(rule);
                         break;
                     }
                 }
                 else
                 {
                     rule->unapply();
+                    if (debPlugin) debPlugin->onUnapply(rule);
                     changes.pop();
                     if (not temporalChanges.empty() and temporalChanges.top() == rule)
                     {
@@ -88,7 +102,13 @@ std::shared_ptr<graph::Forest> solver::BranchingSolver::solve()
             auto rule = isApplicable(instance);
             if (rule != nullptr)
             {
+                if (std::dynamic_pointer_cast<DebugAssertFalseRule>(rule) != nullptr and debPlugin)
+                {
+                    debPlugin->onApply(rule);
+                    debPlugin->onEnd();
+                }
                 rule->apply();
+                if (debPlugin) debPlugin->onApply(rule);
                 changes.push(rule);
                 if (std::dynamic_pointer_cast<PairEqualRule>(rule) != nullptr)
                 {
--- src/Solver/BranchingSolver.hpp
+++ src/Solver/BranchingSolver.hpp
@@ -3,6 +3,7 @@
 
 #include "AbstractSolver.hpp"
 #include "Rule/AbstractRule.hpp"
+#include  "DebugPlugin.hpp"
 
 #include <stack>
 
@@ -18,12 +19,17 @@ class BranchingSolver : public AbstractSolver
     /// \brief stores all applied rules that have to be revoked to get the final solution
     std::stack<std::shared_ptr<AbstractRule>> temporalChanges = std::stack<std::shared_ptr<AbstractRule>>();
 
+    /// \brief A debug plugin, nullptr for no additional debug info
+    std::shared_ptr<DebugPlugin> debPlugin = nullptr;
+
   public:
     explicit BranchingSolver(const std::shared_ptr<graph::Instance>& instance);
 
     ~BranchingSolver() override = default;
 
     std::shared_ptr<graph::Forest> solve() override;
+
+    std::shared_ptr<DebugPlugin>& DebPlugin();
 };
 
 }  //namespace solver
