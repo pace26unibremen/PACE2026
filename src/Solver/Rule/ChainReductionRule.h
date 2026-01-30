4a27ac1e1d3d852b133b782306f62779c9b6242e
André Kaufmann <an_ka1@uni-bremen.de>
2026-01-30 03:43:32 +0100

    doAction, apply implementiert

--- src/Solver/Rule/ChainReductionRule.h
+++ src/Solver/Rule/ChainReductionRule.h
@@ -5,6 +5,7 @@
 #ifndef PACE2026_CHAINREDUCTIONRULE_H
 #define PACE2026_CHAINREDUCTIONRULE_H
 #include "AbstractRule.hpp"
+#include "../Action/DeleteNodeActionInChains.h"
 #include "../Action/DeleteEdgeAction.hpp"
 #include <vector>
 #include <stack>
@@ -14,18 +15,30 @@ namespace solver
     class ChainReductionRule : public AbstractRule
     {
     protected:
+        /// \brief First Tree
         std::shared_ptr<graph::Forest> T1;
+
+        /// \brief Second Tree
         std::shared_ptr<graph::Forest> T2;
+
+        /// \brief List of chains within the two trees
         std::vector<std::vector<std::vector<int>>> chains;
-        std::stack<AbstractAction> changes;
+
+        /// \brief Amount of changes done
+        std::stack<solver::DeleteNodeActionInChains> changes;
 
     public:
+        /// \param T1 First Tree
+        /// \param T2 Second Tree
+        /// \param T3 Chains between those two trees in the paramater
         ChainReductionRule(const std::shared_ptr<graph::Forest>& T1, const std::shared_ptr<graph::Forest>& T2,
                            const std::vector<std::vector<std::vector<int>>>& chains);
+        /// \brief Apply the Chain Reduction rule onto the two Trees
         void apply() override;
-
+        /// \brief Restore the original state before the application of the chain reduction rule
         void unapply() override;
 
+        /// \brief Determine if there are chains between the two trees.
         static std::shared_ptr<AbstractRule> isApplicable(const std::shared_ptr<graph::Forest>& T1,
             const std::shared_ptr<graph::Forest>& T2);
     };
