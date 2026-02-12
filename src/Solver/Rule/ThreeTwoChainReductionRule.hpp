78f1ad3c948f0ea539003fb2a8ba0969c2fcff50
André Kaufmann <an_ka1@uni-bremen.de>
2026-02-12 19:54:42 +0100

    Adding classes for this issue, also fixing double semicolon

---
+++ src/Solver/Rule/ThreeTwoChainReductionRule.hpp
@@ -0,0 +1,19 @@
+//
+// Created by kaufm on 12.02.2026.
+//
+
+#ifndef PACE2026_THREETWOCHAINREDUCTIONRULE_HPP
+#define PACE2026_THREETWOCHAINREDUCTIONRULE_HPP
+#include "AbstractRule.hpp"
+
+namespace solver
+{
+
+    class ThreeTwoChainReductionRule : public AbstractRule
+    {
+
+    };
+
+}  //namespace solver
+
+#endif  //PACE2026_THREETWOCHAINREDUCTIONRULE_HPP
