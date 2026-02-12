f8a7b9493cab7de19bcfb481f7f274c50c59c791
André Kaufmann <an_ka1@uni-bremen.de>
2026-02-12 21:08:48 +0100

    Base class structure constructed

--- src/Solver/Rule/ThreeTwoChainReductionRule.cpp
+++ src/Solver/Rule/ThreeTwoChainReductionRule.cpp
@@ -4,5 +4,31 @@
 
 #include "ThreeTwoChainReductionRule.hpp"
 
-namespace solver
-{}  //namespace solver
+solver::ThreeTwoChainReductionRule::ThreeTwoChainReductionRule(
+    const std::shared_ptr<graph::Instance>& instance,
+    const std::shared_ptr<Context>& context,):AbstractRule(instance,context)
+{
+
+}
+
+int solver::ThreeTwoChainReductionRule::apply()
+{
+    return 0;
+}
+
+void solver::ThreeTwoChainReductionRule::unapply()
+{
+
+}
+
+std::shared_ptr<solver::AbstractRule>
+solver::ThreeTwoChainReductionRule::isApplicable(const std::shared_ptr<graph::Instance>& instance,
+                                                      const std::shared_ptr<Context>& context)
+{
+
+}
+
+std::string solver::ThreeTwoChainReductionRule::name() const
+{
+    return "ThreeTwoChainReductionRule";
+}
