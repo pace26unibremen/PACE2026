92d6564d5783cb086c03e12a153d1ce73d881f58
jschramm <jschramm@uni-bremen.de>
2026-02-19 11:48:05 +0100

    update action and plugin doc

--- src/Solver/Action/AbstractAction.hpp
+++ src/Solver/Action/AbstractAction.hpp
@@ -4,10 +4,18 @@
 namespace solver
 {
 
+/// \brief This is a base class for actions.
+/// Actions are atomic operations on a forest.
+/// They are intended to be both doable and undoable.
 class AbstractAction
 {
   public:
+    virtual ~AbstractAction() = default;
+
+    /// \brief do the action
     virtual void doAction() = 0;
+
+    /// \brief undo the action
     virtual void undoAction() = 0;
 };
 
