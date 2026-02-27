f5cba3381c3dd860615383b79cf0faa635a54e97
jschramm <jschramm@uni-bremen.de>
2026-02-27 12:05:27 +0100

    removed active rules and benchmark

--- tests/BranchingSolverSimpleTests.cpp
+++ tests/BranchingSolverSimpleTests.cpp
@@ -24,21 +24,11 @@ TEST_CASE("BranchingSolver on Tiny Test Set", "[BranchingSolver, Tiny]")
     for (const std::string& f : {"tiny01.nw","tiny02.nw","tiny03.nw","tiny04.nw","tiny05.nw",
                              "tiny06.nw","tiny07.nw","tiny08.nw","tiny09.nw","tiny10.nw"})
     {
-        BENCHMARK("Benchmark solve " + f)
+        SECTION("solve " + f)
         {
             auto instance = graph::ReadInstance(std::string(RES_DIR) + "tiny/" + f);
             auto solver = solver::BranchingSolver(instance);
-            solver.ActiveRules() = {
-                solver::CutBranchRule::isApplicable,
-                solver::EqualForestsRule::isApplicable,
-                solver::SingleVertexTreePropagationRule::isApplicable,
-                solver::PairUnconnectedBranchingRule::isApplicable,
-                solver::PairEqualRule::isApplicable,
-                solver::PairPathBranchingRule::isApplicable,
-                solver::DebugAssertFalseRule::isApplicable};
-
             auto solution = solver.solve();
-            INFO("Instance: " + f);
             REQUIRE(solution->Roots().size() == instanceToSolutionSize[f]);
         };
     }
